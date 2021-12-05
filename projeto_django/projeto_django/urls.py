e68ac5c14f556d5f133473d51369c2eb9b51de9d
Wendell0liveira <wendellshadow13@gmail.com>
2021-12-05 10:20:45 -0300

    agr vai pra página inicial direto

--- projeto_django/projeto_django/urls.py
+++ projeto_django/projeto_django/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
+from corrida.views import index
 urlpatterns = [
+    path("",index,name='pagina inicial'),
     path(r"corrida/", include("corrida.urls")),
     path('admin/', admin.site.urls)
 ]
